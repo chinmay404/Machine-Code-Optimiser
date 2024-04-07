e0324e3efe60c279c770f6086a69e94e1ef2d05e
chinmay404 <92822013+chinmay404@users.noreply.github.com>
2024-04-07 19:29:35 +0530

    Update main.py

--- machine_code_optimiser/main.py
+++ machine_code_optimiser/main.py
@@ -10,12 +10,7 @@ from custom_pasrser import *
 
 css = '''
 <style>
-    .element-container:has(>.stTextArea), .stTextArea {
-        width: 800px !important;
-    }
-    .stTextArea textarea {
-        height: 400px;
-    }
+
 </style>
 '''
 
@@ -28,23 +23,17 @@ def main():
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     st.header("MACHINE CODE OPTIMISER")
-    # user_question = st.text_input("Ask a question :")
+    st.divider()
+    st.subheader('Machine Code Optimizer is a Streamlit web application that utilizes language models to optimize machine code. This tool allows users to upload machine code files, specify a machine name, and receive optimized code as well as cycle time analysis.')
     with st.sidebar:
         uploaded_file = st.file_uploader("Upload", accept_multiple_files=False)
         machine = st.text_input("Enter Machine Name :")
         button = st.button("Process")
-    # if user_question:
-    #     ans = llm_response(user_question)
-        # formatted_ans = parse_llm_response(ans)
-        # st.write(f"Your question: {user_question}")
-        # st.write(f"Answer: {ans}")  
     if uploaded_file is not None and machine:
         try:
             file_data = uploaded_file.read().decode('utf-8')
             processed_data = file_data
             if button: 
-                # st.write("**Processed Data:**")  
-                # st.text(file_data)
                 try:
                     llm_response(file_data,machine)
                 except Exception as E:
@@ -55,30 +44,6 @@ def main():
     else:
         st.error(f"Some Feilds Are Missing")
 
-    # response = lm(messages)
-
-
-
-    
-
-# def parse_llm_response(llm_output):
-#     parsed_text = ""
-#     essential_sections = ["previous code", "new optimised code", "cycle time difference"]
-
-#     for section in essential_sections:
-#         section_start = llm_output.lower().find(section)
-#         if section_start != -1:
-#             section_end = llm_output.find("\n", section_start)
-#             if section_end != -1:
-#                 section_content = llm_output[section_start:section_end].strip()
-#                 parsed_text += f"**{section.title()}**:\n{section_content}\n\n"
-#         else:
-#             return None
-
-#     if parsed_text:
-#         return parsed_text
-#     else:
-#         return None
 
 
 if __name__ == '__main__':
